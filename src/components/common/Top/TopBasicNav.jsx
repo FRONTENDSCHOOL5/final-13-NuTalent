eef4ad5b613149fb65692f1706b8ee553423af68
suminson <sabi1111@nate.com>
2023-06-15 14:09:30 +0900

    Feat: TopMenu 공통 컴포넌트 구현 (#10)

---
+++ src/components/common/Top/TopBasicNav.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import styled from 'styled-components';
+import arrowLeft from '../../../assets/img/icon-arrow-left.svg';
+import optionImg from '../../../assets/img/icon-more-vertical.svg';
+
+const TopDiv = styled.div`
+  width: 39rem;
+  height: 4.8rem;
+  display: flex;
+  box-sizing: border-box;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 1.6rem;
+`;
+const ArrowLeftBtn = styled.button`
+  background-color: inherit;
+  border: none;
+  width: 2.2rem;
+  height: 2.2rem;
+  background-image: url(${arrowLeft});
+`;
+const OptionBtn = styled.button`
+  background-color: inherit;
+  border: none;
+  width: 2.4rem;
+  height: 2.4rem;
+  background-image: url(${optionImg});
+`;
+export default function TopBasicNav() {
+  return (
+    <TopDiv>
+      <ArrowLeftBtn />
+      <OptionBtn />
+    </TopDiv>
+  );
+}
