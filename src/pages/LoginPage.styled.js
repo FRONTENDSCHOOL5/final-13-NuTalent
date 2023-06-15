e1c025824fc166bd9fddbc5d3aa31fd008b158de
soooee4 <soooee4@gmail.com>
2023-06-15 14:31:07 +0900

    Feat: 로그인 페이지 ui 구현 (#17)

---
+++ src/pages/LoginPage.styled.js
@@ -0,0 +1,23 @@
+import { styled } from 'styled-components';
+
+const LoginPageWrap = styled.div`
+  margin: 5.4rem 3.4rem;
+`;
+const PageH2 = styled.h2`
+  font-size: 2.4rem;
+  text-align: center;
+`;
+
+const TextInputBox = styled.div`
+  margin-top: 4rem;
+  margin-bottom: 3rem;
+`;
+
+const LoginCreateAccount = styled.a`
+  display: block;
+  margin-top: 20px;
+  color: #767676;
+  text-align: center;
+`;
+
+export { LoginPageWrap, PageH2, TextInputBox, LoginCreateAccount };
