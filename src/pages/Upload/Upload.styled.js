e16cf0243491e6932012247219bceaaabce367e8
Seongeun Lee <tjsrms1227@gmail.com>
2023-06-16 18:56:48 +0900

    Feat: 게시글 작성 페이지 UI 구현 (#28)

---
+++ src/pages/Upload/Upload.styled.js
@@ -0,0 +1,59 @@
+/* eslint-disable */
+import { styled } from 'styled-components';
+import fileButton from '../../assets/img/upload-file.svg';
+
+export const Form = styled.form`
+  width: 100%;
+  min-height: calc(100vh - 4.8rem);
+  position: relative;
+  padding: 3.2rem 1.6rem 2rem 7rem;
+  border-top: 0.05rem solid var(--sub-grey);
+
+  &::before {
+    content: '';
+    display: block;
+    width: 4.2rem;
+    height: 4.2rem;
+    position: absolute;
+    top: 2rem;
+    left: 1.6rem;
+    background: ${(props) => `url(${props.userImg})`} no-repeat center / cover;
+    border-radius: 50%;
+  }
+`;
+
+export const Textarea = styled.textarea`
+  width: 100%;
+  resize: none;
+  font-size: 1.4rem;
+  font-family: inherit;
+  border: none;
+  outline: none;
+  padding: 0;
+  margin-bottom: 1.6rem;
+
+  &::placeholder {
+    color: var(--sub-grey);
+  }
+`;
+
+export const Image = styled.img`
+  display: block;
+  width: 100%;
+  aspect-ratio: 4/3;
+  margin-bottom: 1.2rem;
+  border-radius: 1rem;
+`;
+
+export const FileLabel = styled.label`
+  width: 5rem;
+  height: 5rem;
+  position: fixed;
+  bottom: 2rem;
+  right: 1.6rem;
+  background-image: url(${fileButton});
+`;
+
+export const FileInput = styled.input`
+  display: none;
+`;
