b14f859f7285cece7886c34743210f5898ab1534
Seongeun Lee <tjsrms1227@gmail.com>
2023-06-19 09:19:34 +0900

    # 게시글 작성 페이지 기능 구현 (#28)
    - 이미지 파일 유효성 검사 구현
    - 게시글 업로드 기능 구현
    - 현재 토큰은 직접 입력해서 사용, 추후 변경
    - 라우팅 처리 필요

--- src/pages/Upload/Upload.styled.js
+++ src/pages/Upload/Upload.styled.js
@@ -2,24 +2,21 @@
 import { styled } from 'styled-components';
 import fileButton from '../../assets/img/upload-file.svg';
 
-export const Form = styled.form`
+export const Section = styled.section`
   width: 100%;
   min-height: calc(100vh - 4.8rem);
   position: relative;
   padding: 3.2rem 1.6rem 2rem 7rem;
   border-top: 0.05rem solid var(--sub-grey);
+`;
 
-  &::before {
-    content: '';
-    display: block;
-    width: 4.2rem;
-    height: 4.2rem;
-    position: absolute;
-    top: 2rem;
-    left: 1.6rem;
-    background: ${(props) => `url(${props.userImg})`} no-repeat center / cover;
-    border-radius: 50%;
-  }
+export const ProfileImg = styled.img`
+  width: 4.2rem;
+  height: 4.2rem;
+  position: absolute;
+  top: 2rem;
+  left: 1.6rem;
+  border-radius: 50%;
 `;
 
 export const Textarea = styled.textarea`
@@ -37,7 +34,7 @@ export const Textarea = styled.textarea`
   }
 `;
 
-export const Image = styled.img`
+export const PostImage = styled.img`
   display: block;
   width: 100%;
   aspect-ratio: 4/3;
