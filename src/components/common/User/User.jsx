ad64e093809c60483d91a3a6319eb242f59daf5e
Seongeun Lee <tjsrms1227@gmail.com>
2023-06-20 16:51:30 +0900

    #Feat: 프로필 페이지 UI 구현 완료

--- src/components/common/User/User.jsx
+++ src/components/common/User/User.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import * as S from './User.styled';
-import defaultProfileImg from '../../../assets/img/basic-profile.svg';
 
-export default function User({size, userName, userId, userImg, type }) {
+export default function User({ size, userName, userId, userImg, type }) {
   return (
     <S.UserStyled>
-      <S.UserImage size={size} src={userImg || defaultProfileImg} alt="사용자 이미지" />
+      <S.UserImage size={size} src={userImg} alt="사용자 이미지" />
       <S.UserDetails>
         <S.UserName>{userName}</S.UserName>
         <S.UserId>@ {userId}</S.UserId>
