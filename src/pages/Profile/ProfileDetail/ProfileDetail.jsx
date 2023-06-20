5f98f286ae7318afccb1578795683138a49c86e7
Seongeun Lee <tjsrms1227@gmail.com>
2023-06-20 16:51:30 +0900

    Feat: 프로필 페이지 UI 구현 완료 (#38)

--- src/pages/Profile/ProfileDetail/ProfileDetail.jsx
+++ src/pages/Profile/ProfileDetail/ProfileDetail.jsx
@@ -1,5 +1,117 @@
-import React from 'react';
+/* eslint-disable */
+import React, { useState } from 'react';
+import TopBasicNav from '../../../components/common/Top/TopBasicNav';
+import ProductItem from '../../../components/common/ProductItem/ProductItem';
+import PostItem from '../../../components/common/PostItem/PostItem';
+import TabMenu from '../../../components/common/Tabmenu/TabMenu';
+import StyledBtn from '../../../components/common/Button/Button';
 
-export default function MyProfile() {
-  return <div>MyProfile</div>;
+import * as S from './ProfileDetail.styled';
+
+export default function Profile() {
+  const [view, setView] = useState('list');
+  const viewHandler = () => {
+    view === 'list' ? setView('album') : setView('list');
+  };
+
+  const Dummy = {
+    followers: '100',
+    followings: '100',
+    userName: '사용자 이름',
+    userId: 'user-id',
+    userIntro: 'XXXXXXXX',
+    userImg: 'https://picsum.photos/200',
+  };
+
+  const postDummy = {
+    postDate: '2022-01-01',
+    postImg: 'https://picsum.photos/200',
+    postLike: '100',
+    postMessage: '30',
+    postText: 'XXXXXXXX',
+  };
+
+  const productDummy = {
+    itemName: 'XXXXXXXX',
+    price: '100',
+    itemImg: 'https://picsum.photos/200',
+  };
+
+  return (
+    <>
+      <TopBasicNav />
+      <S.Container>
+        <S.ProfileSection>
+          <S.ProfileWrap>
+            <S.followWrap>
+              <p>{Dummy.followers}</p>
+              <p>followers</p>
+            </S.followWrap>
+            <S.ProfileImg src={Dummy.userImg} alt="프로필 사진" />
+            <S.followWrap>
+              <p>{Dummy.followings}</p>
+              <p>followings</p>
+            </S.followWrap>
+          </S.ProfileWrap>
+          <S.UserName>{Dummy.userName}</S.UserName>
+          <S.UserId>@ {Dummy.userId}</S.UserId>
+          <S.UserIntro>{Dummy.userIntro}</S.UserIntro>
+          <S.UserBtnWrap>
+            <S.messageButton></S.messageButton>
+            <StyledBtn size="m">팔로우</StyledBtn>
+            <S.shareButton></S.shareButton>
+          </S.UserBtnWrap>
+          {/* 언팔로우일 때, 내 페이지 일 때
+          <S.UserBtnWrap>
+            <S.messageButton></S.messageButton>
+            <StyledBtn size="m" color='outlineGrey'>언팔로우</StyledBtn>
+            <S.shareButton></S.shareButton>
+          </S.UserBtnWrap>
+          <S.UserBtnWrap>
+            <StyledBtn size="m" color='outline'>프로필 수정</StyledBtn>
+            <StyledBtn size="m" color='outline'>상품 등록</StyledBtn>
+          </S.UserBtnWrap> 
+          */}
+        </S.ProfileSection>
+        <S.ProductSection>
+          <S.StyledH2>판매 중인 상품</S.StyledH2>
+          <S.ProductList>
+            <li>
+              <ProductItem
+                itemName={productDummy.itemName}
+                price={productDummy.price}
+                itemImg={productDummy.itemImg}
+              />
+            </li>
+          </S.ProductList>
+        </S.ProductSection>
+        <S.PostSection>
+          <S.PostTop>
+            <S.viewButton view={view} type="button"></S.viewButton>
+            <S.viewButton view={view} type="button"></S.viewButton>
+          </S.PostTop>
+          <S.PostList view={view}>
+            <li>
+              <PostItem
+                postDate={postDummy.postDate}
+                postImg={postDummy.postImg}
+                postLike={postDummy.postLike}
+                postMessage={postDummy.postMessage}
+                postText={postDummy.postText}
+                userId={Dummy.userId}
+                userImg={Dummy.userImg}
+                userName={Dummy.userName}
+              />
+            </li>
+            {/* album view일 때
+             <li>
+              <img src={postDummy.postImg} alt="" />
+            </li> 
+            */}
+          </S.PostList>
+        </S.PostSection>
+      </S.Container>
+      <TabMenu />
+    </>
+  );
 }
---
+++ src/pages/Profile/ProfileDetail/ProfileDetail.styled.js
@@ -0,0 +1,178 @@
+import { css, styled } from 'styled-components';
+
+/* eslint-disable */
+import messageIcon from '../../../assets/img/icon-message-circle.svg';
+import shareIcon from '../../../assets/img/icon-share.svg';
+import listViewOn from '../../../assets/img/icon-post-list-on.svg';
+import listViewOff from '../../../assets/img/icon-post-list-off.svg';
+import AlbumViewOn from '../../../assets/img/icon-post-album-on.svg';
+import AlbumViewOff from '../../../assets/img/icon-post-album-off.svg';
+/* eslint-enable */
+
+export const Container = styled.div`
+  background-color: #f2f2f2;
+  line-height: 1.2;
+  padding: 4.8rem 0;
+
+  & > * {
+    margin-bottom: 1rem;
+    background-color: #fff;
+    border-width: 0.05rem 0;
+    border-color: var(--sub-grey);
+    border-style: solid;
+
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+`;
+
+// Profile section
+export const ProfileSection = styled.section`
+  padding: 3rem 0;
+  text-align: center;
+`;
+
+export const ProfileWrap = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  gap: 3.5rem;
+  margin-bottom: 1.6rem;
+`;
+
+export const ProfileImg = styled.img`
+  width: 11rem;
+  height: 11rem;
+  border-radius: 50%;
+`;
+
+export const followWrap = styled.div`
+  display: inline-block;
+
+  & > p:first-child {
+    font-size: 1.8rem;
+    font-weight: 700;
+    margin-bottom: 0.6rem;
+  }
+
+  & > p:last-child {
+    color: var(--main-grey);
+  }
+`;
+
+export const UserName = styled.p`
+  font-size: 1.6rem;
+  font-weight: 700;
+  margin-bottom: 0.6rem;
+`;
+
+export const UserId = styled.p`
+  margin-bottom: 1.6rem;
+  font-size: 1.2rem;
+  color: var(--main-grey);
+`;
+
+export const UserIntro = styled.p`
+  margin-bottom: 2.4rem;
+  font-size: 1.4rem;
+  color: var(--main-grey);
+`;
+
+export const UserBtnWrap = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+`;
+
+const circleBtn = css`
+  width: 3.4rem;
+  height: 3.4rem;
+  border-radius: 50%;
+  border: 0.1rem solid var(--sub-grey);
+`;
+
+export const messageButton = styled.button`
+  ${circleBtn}
+  background: url(${messageIcon}) no-repeat center;
+`;
+
+export const shareButton = styled.button`
+  ${circleBtn}
+  background: url(${shareIcon}) no-repeat center;
+`;
+
+// Product section
+export const ProductSection = styled.section`
+  padding: 2rem 1.6rem;
+`;
+
+export const StyledH2 = styled.h2`
+  font-size: 1.6rem;
+  font-weight: 700;
+  margin-bottom: 1.6rem;
+`;
+
+export const ProductList = styled.ul`
+  display: flex;
+  flex-direction: row;
+  gap: 1rem;
+  padding-bottom: 0.5rem;
+  overflow-x: auto;
+`;
+
+// Post section
+export const PostSection = styled.section`
+  ${(props) => props.view}
+`;
+
+export const PostTop = styled.div`
+  height: 4.4rem;
+  padding: 0.9rem 1.6rem;
+  border-bottom: 0.05rem solid var(--sub-grey);
+  display: flex;
+  gap: 1.6rem;
+`;
+
+export const viewButton = styled.button`
+  width: 2.6rem;
+  height: 2.6rem;
+
+  &:first-child {
+    margin-left: auto;
+    background: url(${(props) => props.view === 'list' ? listViewOn : listViewOff})
+      no-repeat center;
+  }
+
+  &:last-child {
+    background: url(${(props) => props.view === 'album' ? AlbumViewOn : AlbumViewOff})
+      no-repeat center;
+  }
+`;
+
+const listView = css`
+  & > li {
+    margin-bottom: 2rem;
+  }
+`;
+
+const albumView = css`
+  display: grid;
+  grid-template-columns: repeat(3, minmax(0, 1fr));
+  gap: 0.8rem;
+`;
+
+export const PostList = styled.ul`
+  padding: 1.6rem;
+  min-width: 32rem;
+  max-width: 64rem;
+  margin: 0 auto;
+
+  & > li > img {
+    width: 100%;
+  }
+
+  ${(props) => (props.view === 'list' ? listView : albumView)}
+`;
