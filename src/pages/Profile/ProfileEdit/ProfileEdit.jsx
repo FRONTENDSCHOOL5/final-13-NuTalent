3ff97dca7aa04b2fe3a86b9273550ad16b424c56
eunchokang <wkdb0425@gmail.com>
2023-06-21 09:36:35 +0900

    Feat: 프로필 설정페이지 사용자 정보 불러오기 (#50)
    - api 연결하여 사용자 이름, 계정ID, 소개 불러오기
    - 추후 프로필 이미지 불러오기 필요
    - 추후 id, token값 동적으로 받아오기 필요

--- src/pages/Profile/ProfileEdit/ProfileEdit.jsx
+++ src/pages/Profile/ProfileEdit/ProfileEdit.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-// import { instance } from '../../../util/api/axiosInstance';
+import React, { useState, useEffect } from 'react';
+import { instance } from '../../../util/api/axiosInstance';
 import {
   JoinMembersWrap,
   // PageH2,
@@ -20,6 +20,46 @@ import TopBasicNav from '../../../components/common/Top/TopBasicNav';
 import uploadImage from '../../../assets/img/upload-file.svg';
 
 export default function ProfileEdit() {
+  const [profileImage, setProfileImage] = useState('');
+  const [userName, setUserName] = useState('');
+  const [userId, setUserId] = useState('');
+  const [description, setDescription] = useState('');
+
+  const dummyUserId = process.env.REACT_APP_USER_ID;
+  const dummyUserToken = process.env.REACT_APP_USER_TOKEN;
+
+  console.log('dummyUserId', dummyUserId);
+  console.log('dummyUserToken', dummyUserToken);
+
+  useEffect(() => {
+    async function getProfile() {
+      const getProfileRes = await instance.get(
+        // TODO: id 동적으로 값을 받아오도록 변경 필요
+        `/profile/${dummyUserId}`,
+        {
+          params: {
+            image: profileImage,
+            userName: userName,
+            userId: userId,
+            description: description,
+          },
+          headers: {
+            // TODO: token값 동적으로 받아오도록 변경 필요
+            Authorization: `Bearer ${dummyUserToken}`,
+            'Content-type': 'application/json',
+          },
+        },
+      );
+      console.log(getProfileRes);
+
+      setProfileImage(getProfileRes.data.profile.image);
+      setUserName(getProfileRes.data.profile.username);
+      setUserId(getProfileRes.data.profile.accountname);
+      setDescription(getProfileRes.data.profile.intro);
+    }
+
+    getProfile();
+  }, []);
   return (
     <>
       <TopBasicNav />
@@ -48,7 +88,7 @@ export default function ProfileEdit() {
             type="text"
             className="user-name"
             // placeholder="2~10자 이내여야 합니다."
-            // value={userName}
+            value={userName}
             // onChange={handleUserNameChange}
             // onBlur={handleUserNameBlur}
           >
@@ -61,7 +101,7 @@ export default function ProfileEdit() {
             type="text"
             className="user-id"
             // placeholder="영문, 숫자, 특수문자(.),(_)만 사용 가능합니다."
-            // value={userId}
+            value={userId}
             // onChange={handleUserIdChange}
             // onBlur={handleUserIdBlur}
           >
@@ -72,7 +112,7 @@ export default function ProfileEdit() {
             type="text"
             className="user-description"
             // placeholder="자신과 판매할 상품에 대해 소개해주세요!"
-            // value={description}
+            value={description}
             // onChange={handleDescriptionChange}
             // onBlur={handleDescriptionBlur}
           >
