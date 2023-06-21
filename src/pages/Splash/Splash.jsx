1c22ecf5cf597a6ae9b871c72c2f06364579d15e
eunchokang <wkdb0425@gmail.com>
2023-06-21 16:55:10 +0900

    Feat: 3초후 Splash페이지 -> Into페이지로 이동 (#58)
    - 3초후 Splash페이지 -> Into페이지로 이동

--- src/pages/Splash/Splash.jsx
+++ src/pages/Splash/Splash.jsx
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import splash from '../../assets/img/Splash-문구.svg';
 import { SplashDiv } from './Splash.styled';
+import { useNavigate } from 'react-router-dom';
 
 export default function Splash() {
+  const navigate = useNavigate;
+
+  useEffect(() => {
+    setTimeout(function () {
+      // TODO: 부드러운 화면전환이 필요합니다...
+      navigate('/intro');
+    }, 3000);
+  }, []);
   return (
     <>
       <SplashDiv>
