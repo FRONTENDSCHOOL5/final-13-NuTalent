68623a16ef4a5e43a9f70da91fb486b5048f1850
eunchokang <wkdb0425@gmail.com>
2023-06-21 16:54:08 +0900

    Style: Splash 페이지 UI 구현 (#58)
    - Splash 페이지 UI 구현

--- src/pages/Splash/Splash.jsx
+++ src/pages/Splash/Splash.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
+import splash from '../../assets/img/Splash-문구.svg';
+import { SplashDiv } from './Splash.styled';
 
 export default function Splash() {
-  return <div>Splash</div>;
+  return (
+    <>
+      <SplashDiv>
+        {/* TODO: 로고가 움직이게 할수 있으면 좋을 것 같습니다. */}
+        <img src={splash} />
+      </SplashDiv>
+    </>
+  );
 }
