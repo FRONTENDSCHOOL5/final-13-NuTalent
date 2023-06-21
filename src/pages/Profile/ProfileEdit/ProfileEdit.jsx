255ae01233b0b104cf913f39966a36ad41f2d2f9
eunchokang <wkdb0425@gmail.com>
2023-06-21 11:31:16 +0900

    Feat: 프로필 수정 api 연결 (#50)
    - 유효성 검사 통과하지 못하면 제출 버튼 비활성
    - 프로필 수정 api 연결
    - api통신에 실패하면 에러 메시지를 해당 칸에 띄워주도록 구현
    - api통신에 성공하면 Profile 페이지로 이동

--- src/pages/Profile/ProfileEdit/ProfileEdit.jsx
+++ src/pages/Profile/ProfileEdit/ProfileEdit.jsx
@@ -10,21 +10,27 @@ import {
   ProfileUploadInput,
   ProfileUploadLabel,
   ProfileUploadDiv,
-  JoinMembersNextButton,
   TextInputBox,
-  // ErrorMessage,
+  ErrorMessage,
 } from './ProfileEdit.styled';
 import TextActiveInput from '../../../components/common/TextActiveInput/TextActiveInput';
 import TopUploadNav from '../../../components/common/Top/TopUploadNav';
 // import StyledBtn from '../../../components/common/Button/Button';
 // import profileDefault from '../../../assets/img/basic-profile-img-.svg';
 import uploadImage from '../../../assets/img/upload-file.svg';
+import { useNavigate } from 'react-router-dom';
 
 export default function ProfileEdit() {
   const [profileImage, setProfileImage] = useState('');
   const [userName, setUserName] = useState('');
+  const [isUserNameInvalid, setIsUserNameInvalid] = useState(false);
+  const [userIdErrorMessage, setUserIdErrorMessage] = useState('');
   const [userId, setUserId] = useState('');
+  const [isUserIdInvalid, setIsUserIdInvalid] = useState(false);
   const [description, setDescription] = useState('');
+  const [isDescriptionInvalid, setIsDescriptionInvalid] = useState(false);
+  const navigate = useNavigate();
+  const idRegExp = /^[a-zA-Z0-9_.]+$/;
 
   const dummyUserId = process.env.REACT_APP_USER_ID;
   const dummyUserToken = process.env.REACT_APP_USER_TOKEN;
@@ -58,9 +64,98 @@ export default function ProfileEdit() {
 
     getProfile();
   }, []);
+
+  const handleUserNameChange = (e) => {
+    const currentUserName = e.target.value.trim();
+    setUserName(currentUserName);
+  };
+
+  const handleUserIdChange = (e) => {
+    const currentUserId = e.target.value.trim();
+    setUserId(currentUserId);
+  };
+
+  const handleDescriptionChange = (e) => {
+    const currentDescription = e.target.value.trim();
+    setDescription(currentDescription);
+  };
+
+  // focus를 잃으면 실행
+  const handleUserNameBlur = () => {
+    // 유효성 검사
+    // 사용자 이름 : 2~10자 이내여야합니다.
+    if (userName.length < 2 || userName.length > 10) {
+      setIsUserNameInvalid(true);
+      console.log(isUserNameInvalid);
+    } else {
+      setIsUserNameInvalid(false);
+      console.log(isUserNameInvalid);
+    }
+  };
+
+  // focus를 잃으면 실행
+  const handleUserIdBlur = () => {
+    // 유효성 검사
+    // 계정ID: 영문, 숫자, 특수문자(.), (_)만 사용가능합니다.
+    if (!idRegExp.test(userId)) {
+      setIsUserIdInvalid(true);
+      setUserIdErrorMessage(
+        '*계정ID는 영문, 숫자, 밑줄, 마침표만 사용할 수 있습니다.',
+      );
+      console.log(isUserIdInvalid);
+    } else {
+      setIsUserIdInvalid(false);
+      console.log(isUserIdInvalid);
+    }
+  };
+
+  // focus를 잃으면 실행
+  const handleDescriptionBlur = () => {
+    // 유효성 검사
+    // 소개: 글이 비어있지 않으면 유효성검사 통과
+    if (!description) {
+      setIsDescriptionInvalid(true);
+      console.log(isDescriptionInvalid);
+    } else {
+      setIsDescriptionInvalid(false);
+      console.log(isDescriptionInvalid);
+    }
+  };
+
+  async function handleClick() {
+    try {
+      const user = JSON.stringify({
+        user: {
+          username: userName,
+          accountname: userId,
+          intro: description,
+          image: profileImage,
+        },
+      });
+      const res = await instance.put('/user', user, {
+        headers: {
+          Authorization: `Bearer ${dummyUserToken}`,
+          'Content-type': 'application/json',
+        },
+      });
+
+      console.log(res);
+
+      navigate('/profile');
+    } catch (error) {
+      console.error(error);
+
+      setIsUserIdInvalid(true);
+      setUserIdErrorMessage(`*${error.response.data.message}`);
+    }
+  }
+
   return (
     <>
-      <TopUploadNav />
+      <TopUploadNav
+        onClick={handleClick}
+        disabled={isUserIdInvalid || isUserNameInvalid || isDescriptionInvalid}
+      />
       <JoinMembersWrap>
         <TextInputBox
         // onSubmit={handleSubmit}
@@ -87,8 +182,8 @@ export default function ProfileEdit() {
             className="user-name"
             // placeholder="2~10자 이내여야 합니다."
             value={userName}
-            // onChange={handleUserNameChange}
-            // onBlur={handleUserNameBlur}
+            onChange={handleUserNameChange}
+            onBlur={handleUserNameBlur}
           >
             사용자 이름
           </TextActiveInput>
@@ -100,33 +195,25 @@ export default function ProfileEdit() {
             className="user-id"
             // placeholder="영문, 숫자, 특수문자(.),(_)만 사용 가능합니다."
             value={userId}
-            // onChange={handleUserIdChange}
-            // onBlur={handleUserIdBlur}
+            onChange={handleUserIdChange}
+            onBlur={handleUserIdBlur}
           >
             계정ID
           </TextActiveInput>
-          {/* {isUserIdInvalid && <ErrorMessage>{userIdErrorMessage}</ErrorMessage>} */}
+          {isUserIdInvalid && <ErrorMessage>{userIdErrorMessage}</ErrorMessage>}
           <TextActiveInput
             type="text"
             className="user-description"
             // placeholder="자신과 판매할 상품에 대해 소개해주세요!"
             value={description}
-            // onChange={handleDescriptionChange}
-            // onBlur={handleDescriptionBlur}
+            onChange={handleDescriptionChange}
+            onBlur={handleDescriptionBlur}
           >
             소개
           </TextActiveInput>
           {/* {isDescriptionInvalid && (
           <ErrorMessage>*내용을 입력해주세요</ErrorMessage>
         )} */}
-          <JoinMembersNextButton
-            type="submit"
-            // disabled={
-            //   isUserIdInvalid || isUserNameInvalid || isDescriptionInvalid
-            // }
-          >
-            내 작품을 세상에 소개하러가기
-          </JoinMembersNextButton>
         </TextInputBox>
       </JoinMembersWrap>
     </>
