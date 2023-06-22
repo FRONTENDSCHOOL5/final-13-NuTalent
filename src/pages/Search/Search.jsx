2873cca931d4f5b7b4ddd3bd7735a7238ed428db
eunchokang <wkdb0425@gmail.com>
2023-06-22 17:25:04 +0900

    Docs: 필요없는 부분 삭제 및 import 순서 변경 (#63)
    - 필요없는 부분 삭제 
    - import 순서 변경

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -1,15 +1,19 @@
 import React, { useState } from 'react';
+
 import { SearchWrap, UserListLi } from './Search.styled';
+
 import TopSearchNav from '../../components/common/Top/TopSearchNav';
 import TabMenu from '../../components/common/Tabmenu/TabMenu';
 import User from '../../components/common/User/User';
+
 import { instance } from '../../util/api/axiosInstance';
+
 import defaultProfileImage from '../../assets/img/basic-profile-img-.svg';
-// import axios from 'axios';
 
 export default function Search() {
   const [keywordForSearchUser, setKeywordForSearchUser] = useState('');
   const [data, setData] = useState([]);
+
   const token = process.env.REACT_APP_USER_TOKEN;
 
   const handleSearchUser = async (e) => {
