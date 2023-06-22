b19a6634af4827981dfdf46076a7c89fd44f4918
eunchokang <wkdb0425@gmail.com>
2023-06-22 13:47:43 +0900

    Feat: 검색페이지에서 더미데이터를 받아 동적으로 유저 리스트 생성하도록 구현 (#63)
    - 검색페이지 UI 구현
    - 더미데이터를 받아 동적으로 유저리스트 생성하도록 구현

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -1,5 +1,142 @@
 import React from 'react';
+import { SearchWrap, UserListLi } from './Search.styled';
+import TopSearchNav from '../../components/common/Top/TopSearchNav';
+import TabMenu from '../../components/common/Tabmenu/TabMenu';
+import User from '../../components/common/User/User';
 
 export default function Search() {
-  return <div>Search</div>;
+  const data = [
+    {
+      _id: '01',
+      username: 'user1',
+      accountname: '내가 사용자1',
+      following: [],
+      follower: [],
+      followerCount: Number,
+      followingCount: Number,
+      image: 'https://picsum.photos/200/300',
+    },
+    {
+      _id: '02',
+      username: 'user2',
+      accountname: '내가 사용자2',
+      following: [],
+      follower: [],
+      followerCount: Number,
+      followingCount: Number,
+      image: 'https://picsum.photos/200/300',
+    },
+    {
+      _id: '03',
+      username: 'user3',
+      accountname: '내가 사용자3',
+      following: [],
+      follower: [],
+      followerCount: Number,
+      followingCount: Number,
+      image: 'https://picsum.photos/200/300',
+    },
+    {
+      _id: '04',
+      username: 'user4',
+      accountname: '내가 사용자4',
+      following: [],
+      follower: [],
+      followerCount: Number,
+      followingCount: Number,
+      image: 'https://picsum.photos/200/300',
+    },
+    {
+      _id: '05',
+      username: 'user5',
+      accountname: '내가 사용자5',
+      following: [],
+      follower: [],
+      followerCount: Number,
+      followingCount: Number,
+      image: 'https://picsum.photos/200/300',
+    },
+    {
+      _id: '06',
+      username: 'user6',
+      accountname: '내가 사용자6',
+      following: [],
+      follower: [],
+      followerCount: Number,
+      followingCount: Number,
+      image: 'https://picsum.photos/200/300',
+    },
+    {
+      _id: '07',
+      username: 'user7',
+      accountname: '내가 사용자7',
+      following: [],
+      follower: [],
+      followerCount: Number,
+      followingCount: Number,
+      image: 'https://picsum.photos/200/300',
+    },
+    {
+      _id: '08',
+      username: 'user8',
+      accountname: '내가 사용자8',
+      following: [],
+      follower: [],
+      followerCount: Number,
+      followingCount: Number,
+      image: 'https://picsum.photos/200/300',
+    },
+    {
+      _id: '09',
+      username: 'user9',
+      accountname: '내가 사용자9',
+      following: [],
+      follower: [],
+      followerCount: Number,
+      followingCount: Number,
+      image: 'https://picsum.photos/200/300',
+    },
+    {
+      _id: '10',
+      username: 'user10',
+      accountname: '내가 사용자10',
+      following: [],
+      follower: [],
+      followerCount: Number,
+      followingCount: Number,
+      image: 'https://picsum.photos/200/300',
+    },
+    {
+      _id: '11',
+      username: 'user11',
+      accountname: '내가 사용자11',
+      following: [],
+      follower: [],
+      followerCount: Number,
+      followingCount: Number,
+      image: 'https://picsum.photos/200/300',
+    },
+  ];
+  return (
+    <>
+      <TopSearchNav />
+      <SearchWrap>
+        <ul>
+          {data.map((data, index) => {
+            return (
+              <UserListLi key={index}>
+                <User
+                  userName={data.username}
+                  userImg={data.image}
+                  userId={data.accountname}
+                />
+              </UserListLi>
+            );
+          })}
+        </ul>
+      </SearchWrap>
+
+      <TabMenu />
+    </>
+  );
 }
