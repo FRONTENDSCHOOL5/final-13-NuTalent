97edc603c38bf22f192a9f1c15199a77f42838fd
Seongeun Lee <tjsrms1227@gmail.com>
2023-07-22 16:20:56 +0900

    Chore: 리액트 쿼리 패키지 설치 및 초기 설정 (#190)
    - 리액트 쿼리, 리액트 쿼리 데브툴, 리액트 쿼리 린트

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import Router from './routes/Router';
 import { RecoilRoot } from 'recoil';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <div>
-      <RecoilRoot>
-        <Router />
-      </RecoilRoot>
+      <QueryClientProvider client={queryClient}>
+        <RecoilRoot>
+          <Router />
+        </RecoilRoot>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </div>
   );
 }
