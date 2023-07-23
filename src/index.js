4b81959e69d23dc8d8e0588e83a75fb7ed42d4ad
Seongeun Lee <tjsrms1227@gmail.com>
2023-07-23 23:35:58 +0900

    Refactor: axios instance 재사용성 증가 (#190)
    - 인터셉트로 api에 따라 다른 header 추가
    - 앱 시작시 인터셉터에 토큰 추가

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import { GlobalStyle } from './styles/GlobalStyle';
 import { Fragment } from 'react';
+import { RecoilRoot } from 'recoil';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
   <Fragment>
-    <GlobalStyle />
-    <App />
+    <RecoilRoot>
+      <GlobalStyle />
+      <App />
+    </RecoilRoot>
   </Fragment>,
 );
