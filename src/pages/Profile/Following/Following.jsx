b52df546a679ff934f5c89083fed555c3de171dc
soooee4 <soooee4@gmail.com>
2023-08-23 23:37:29 +0900

    Refactor: follower,following-페이지-리액트-쿼리-도입 (#200)

--- src/pages/Profile/Following/Following.jsx
+++ src/pages/Profile/Following/Following.jsx
@@ -1,84 +1,72 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import TopBasicNav from '../../../components/common/Top/TopBasicNav';
 import FollowingUser from './FollowingUser';
 import TabMenu from '../../../components/common/Tabmenu/TabMenu';
 import { UserWrapper } from './Following.styled';
-import { instance } from '../../../util/api/axiosInstance';
 import { useLocation } from 'react-router-dom';
 import { recoilData } from '../../../recoil/atoms/dataState';
 import { useRecoilValue } from 'recoil';
+import { useGetFollowings } from '../../../hooks/react-query/useGetFollowing';
+import { useMutation } from 'react-query';
+import { instance } from '../../../util/api/axiosInstance';
 
 export default function Following() {
-  // 내가 follow 하는 사람들의 list를 저장할 state
-  const [following, setFollowing] = useState([]);
-
   const location = useLocation();
   const { accountName, myAccountName } = location.state;
-  // const token = localStorage.getItem('token');
-
   const token = useRecoilValue(recoilData).token;
-  console.log(token);
-  // useEffect(콜백함수, 의존성 배열)
-  // 의존성 배열의 요소가 변경되면 콜백함수 실행
-  useEffect(() => {
-    getFollowing();
-  }, []);
 
-  /* 
-    2. 버튼 눌렀을 때 getFollowers 가 실행이 되고 서버와 통신해서 setFollowers에 나를 팔로우한 사람들의 데이터를 넣는다.
-    그렇게 되면 빈 배열이 아니라 값이 채워지게 되고, 아래 return 문으로 가게 되면
-  */
-  const getFollowing = async () => {
-    const res = await instance.get(
-      `https://api.mandarin.weniv.co.kr/profile/${accountName}/following?limit=infinite&skip=0 `,
-      {
-        headers: {
-          Authorization: `Bearer ${token}`,
-          'Content-type': 'application/json',
-        },
-      },
-    );
+  // 팔로잉
+  const { followings, queryClient } = useGetFollowings(accountName);
 
-    setFollowing(res.data);
-  };
-
-  // FollowerUser.jsx에서 넘겨받은 userInfo(내가 팔로우 하는 사람의 정보)를 params로 넣는다
-  const followHandler = async (userInfo) => {
-    try {
-      /**
-       * params로 넘겨받은 userInfo에 isfollow(내가 팔로우를 하는지에 대한 유무)가 true일 경우
-       * 해당 userInfo에 있는 accountname을 언팔로우 api의 params로 보내 언팔로우를 진행한다
-       */
-      if (userInfo.isfollow) {
-        await instance.delete(`/profile/${userInfo.accountname}/unfollow `, {
+  // Unfollow mutation
+  const unfollowMutation = useMutation(
+    (userInfo) =>
+      instance.delete(
+        `/profile/${userInfo.accountname}/unfollow?limit=infinite&skip=0`,
+        {
           headers: {
             Authorization: `Bearer ${token}`,
             'Content-type': 'application/json',
           },
-        });
+        },
+      ),
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries(['followings', accountName]);
+      },
+    },
+  );
 
-        //데이터 재조회 => 언팔로우가 끝났으니 getFollower 함수를 실행시켜 팔로우 재조회
-        getFollowing();
-
-        // 넘겨받은 userInfo에 isFollow가 false일 경우 팔로우 api에 accountname을 보내 팔로우 처리
-      } else {
-        await instance.post(
-          `/profile/${userInfo.accountname}/follow`,
-          {},
-          {
-            headers: {
-              Authorization: `Bearer ${token}`,
-              'Content-type': 'application/json',
-            },
+  // Follow mutation
+  const followMutation = useMutation(
+    (userInfo) =>
+      instance.post(
+        `/profile/${userInfo.accountname}/follow`,
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            'Content-type': 'application/json',
           },
-        );
+        },
+      ),
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries(['followings', accountName]);
+      },
+    },
+  );
 
-        // 데이터 재조회 => 팔로우가 끝났으니 getFollowing 함수를 실행히켜 팔로우 재조회
-        getFollowing();
-        console.log('팔로우 성공');
+  const followHandler = async (userInfo) => {
+    try {
+      if (userInfo.isfollow) {
+        await unfollowMutation.mutateAsync(userInfo);
+      } else {
+        await followMutation.mutateAsync(userInfo);
       }
     } catch (error) {
       console.error(error);
+      alert(`${error.response.data.message}`);
     }
   };
 
@@ -86,22 +74,29 @@ export default function Following() {
     <>
       <TopBasicNav>Followings</TopBasicNav>
       <UserWrapper>
-        {/* 3. 이곳에 useState에 채워져있는 배열이 map으로 뿌려지게 되고, 빈 화면이 아니라 유저들의 정보가 화면에 뿌려지게 된다. */}
         {(() => {
-          const myAccount = following.find(
+          if (!followings) return;
+
+          // find 사용하여 내 계정 찾기
+          const myAccount = followings.find(
             (user) => user.accountname === myAccountName,
           );
 
-          const otherFollowing = following.filter((user) => user !== myAccount);
+          // filter 사용하여 다른 사람의 계정만 필터링
+          const otherFollowings = followings.filter(
+            (user) => user !== myAccount,
+          );
 
-          const sortedFollowing = myAccount
-            ? [myAccount, ...otherFollowing]
-            : otherFollowing;
+          // myAccount가 존재하면 배열에 myAccount를 첫번째 요소로 추가한 후 전개문법 사용하여 otherFollowings의 모든 요소들을 추가
+          // myAccount가 없다면 바로 otherFollowings 추가
+          const sortedFollowings = myAccount
+            ? [myAccount, ...otherFollowings]
+            : otherFollowings;
 
-          return sortedFollowing.map((user) => (
+          return sortedFollowings.map((user) => (
             <li key={user._id}>
               <FollowingUser
-                userInfo={user} // 내가 팔로우 하는 사람들 각각의 정보를 넘김
+                userInfo={user}
                 followHandler={followHandler}
                 size={'small'}
                 myAccountName={myAccountName}
