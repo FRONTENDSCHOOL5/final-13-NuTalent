a0ec04fd737a224f541092043cba9c11bbcc76b7
euncho.kang <wkdb0425@gmail.com>
2023-08-26 23:56:57 +0900

    Fix: 팔로우/팔로잉 버튼 작동하도록 코드 수정 (#214)
    Co-authored-by: 이선근 <tjsrms1227@gmail.com>

--- src/hooks/react-query/useFollow.js
+++ src/hooks/react-query/useFollow.js
@@ -1,9 +1,9 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 import { instance } from '../../util/api/axiosInstance';
 
 const follow = async (accountname) => {
-  const res = await instance.post(`/profile/${accountname}/follow`, null);
+  const res = await instance.post(`/profile/${accountname}/follow`);
   return res;
 };
 
@@ -13,9 +13,14 @@ const unfollow = async (accountname) => {
 };
 
 export const useToggleFollow = () => {
+  const queryClient = useQueryClient();
+
   const { mutate: toggleFollowMutate } = useMutation({
-    mutationFn: (profile) => (profile.isfollow ? unfollow() : follow()),
-    onSuccess: (data) => console.log(data),
+    mutationFn: (profile) => (profile.isfollow ? unfollow(profile.accountname) : follow(profile.accountname)),
+    onSuccess: (res) => {
+      const profile = res.data.profile;
+      queryClient.setQueryData(["profile", profile.accountname], profile);
+    },
   });
   return { toggleFollowMutate };
 };
