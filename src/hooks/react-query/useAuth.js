b49f4cbaf4b2c4c78aad338f8783064c896df1cd
euncho.kang <wkdb0425@gmail.com>
2023-08-29 19:35:19 +0900

    Fix: alert창에서 계정ID 사용 여부 알려주기 추가
    - 기존에는 가입 가능한/불가능한 아이디 유효성 검사시 결과 확인이 불가능했음. 그래서 alert창에서 결과 보여주도록 설정
    - 추후 알림창 디자인 완료시 완료된 알림창으로 변경 필요

--- src/hooks/react-query/useAuth.js
+++ src/hooks/react-query/useAuth.js
@@ -4,11 +4,9 @@ import { instance } from '../../util/api/axiosInstance';
 import { useNavigate } from "react-router-dom";
 
 const accountNameValid = async (accountName) => {
-    console.log(accountName);
     const userAccountName = JSON.stringify({
         user: { accountname: accountName, }
     })
-    console.log('userAccountName', userAccountName)
     const data = await instance.post(`/user/accountnamevalid`, userAccountName);
     return data;
 };
@@ -26,8 +24,6 @@ const joinMember = async (user) => {
     })
     const data = await instance.post('/user', userToJoin);
 
-    console.log('지나갑니다.')
-
     return data;
 }
 
@@ -38,7 +34,8 @@ export const useAccountNameValid = () => {
         mutationFn: (accountName) => accountNameValid(accountName),
         onSuccess: (res) => {
             console.log(res);
-            // alert('사용 가능한 계정ID입니다 :)')
+            // TODO: alert창을 추후 디자인 된 알림창으로 변경 필요
+            alert(`${res.data.message}`);
         },
         onError: (err) => {
             console.error(err)
@@ -53,8 +50,7 @@ export const useJoinMember = () => {
 
     const { mutate: JoinMemberMutate } = useMutation({
         mutationFn: (user) => joinMember(user),
-        onSuccess: (res) => {
-            console.log(res);
+        onSuccess: () => {
             alert('회원가입에 성공하였습니다!');
             navigate('/login');
         },
