c7ea33fdaa229bb504d107d2c1febacd06e33111
eunchokang <wkdb0425@gmail.com>
2023-08-31 01:32:26 +0900

    Refactor: 리액트 쿼리 적용 (#225)
    - Search 페이지에서 클라이언트 및 서버 코드를 한꺼번에 작성하여 debounce사용
    - 서버상태 관리 훅을 분리하여 관리
    - 너무 많은 리스트가 나타나면 100개까지만 나타내도록 수정

---
+++ src/hooks/react-query/useSearch.js
@@ -0,0 +1,18 @@
+import { useQuery } from "@tanstack/react-query";
+import { instance } from "../../util/api/axiosInstance";
+
+const getSearchData = async (keyword) => {
+    const res = await instance.get(`/user/searchuser/?keyword=${keyword}`);
+    return res.data;
+}
+
+export const useGetResult = (keyword) => {
+    const { data: searchedResult } = useQuery({
+        queryKey: ['keyword', keyword],
+        queryFn: () => getSearchData(keyword),
+        placeholderData: [],
+        enabled: !!keyword,
+        select: (data) => data.slice(0, 100),
+    })
+    return { searchedResult };
+}
--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 import { SearchWrap, UserListLi } from './Search.styled';
 
@@ -6,68 +6,38 @@ import TopSearchNav from '../../components/common/Top/TopSearchNav';
 import TabMenu from '../../components/common/Tabmenu/TabMenu';
 import User from '../../components/common/User/User';
 
-import { instance } from '../../util/api/axiosInstance';
-
-import { useRecoilValue } from 'recoil';
-import { recoilData } from '../../recoil/atoms/dataState';
-import { debounce } from 'lodash';
+import { useGetResult } from '../../hooks/react-query/useSearch';
 
 export default function Search() {
   const [keywordToSearchUser, setKeywordToSearchUser] = useState('');
-  const [data, setData] = useState([]);
-
-  const token = useRecoilValue(recoilData).token;
-
-  const sendQuery = async () => {
-    if (!keywordToSearchUser) return;
-    const res = await instance.get(
-      `/user/searchuser/?keyword=${keywordToSearchUser}`,
-      {
-        headers: {
-          Authorization: `Bearer ${token}`,
-          'Content-type': 'application/json',
-        },
-      },
-    );
-    setData(res.data);
-  };
-
-  const delayedSearch = useCallback(debounce(() => sendQuery(), 500));
-
-  const handleSearchUserChange = (e) => {
-    setKeywordToSearchUser(e.target.value);
-    delayedSearch();
-  };
-
-  useEffect(() => {
-    sendQuery();
-  }, [keywordToSearchUser]);
+  const { searchedResult } = useGetResult(keywordToSearchUser);
 
   return (
     <>
       <TopSearchNav
         type="text"
         value={keywordToSearchUser}
-        onChange={handleSearchUserChange}
+        onChange={(e) => setKeywordToSearchUser(e.target.value)}
       />
       <SearchWrap>
         <ul>
-          {data.map((data) => {
-            return (
-              <UserListLi key={data._id}>
-                <User
-                  userName={data.username}
-                  userImg={data.image}
-                  userId={data.accountname}
-                  to={`/profile/${data.accountname}`}
-                  state={{ userId: data.accountname }}
-                />
-              </UserListLi>
-            );
-          })}
+          {searchedResult
+            ? searchedResult.map((data) => {
+                return (
+                  <UserListLi key={data._id}>
+                    <User
+                      userName={data.username}
+                      userImg={data.image}
+                      userId={data.accountname}
+                      to={`/profile/${data.accountname}`}
+                      state={{ userId: data.accountname }}
+                    />
+                  </UserListLi>
+                );
+              })
+            : null}
         </ul>
       </SearchWrap>
-
       <TabMenu />
     </>
   );
