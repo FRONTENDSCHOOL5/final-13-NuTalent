906707d61ac58f580151de0e17b37d43f9503779
soooee4 <soooee4@gmail.com>
2023-09-19 21:22:02 +0900

    Refactor: 파일 압축 전후 확인 가능한 console.log 추가

--- src/util/imageCompression.js
+++ src/util/imageCompression.js
@@ -2,6 +2,7 @@ import imageCompression from 'browser-image-compression';
 
 export const handleImageUpload = async (e) => {
   const imageFile = e.target.files[0];
+  console.log(`originalFile size ${imageFile.size / 1024 / 1024} MB`); // 원본 파일크기
 
   const options = {
     maxSizeMB: 0.9,
@@ -11,6 +12,7 @@ export const handleImageUpload = async (e) => {
 
   try {
     const compressedFile = await imageCompression(imageFile, options);
+    console.log(`compressedFile size ${compressedFile.size / 1024 / 1024} MB`); // 압축 후 파일크기
     const fileName = compressedFile.name;
     const image = new File([compressedFile], fileName, { type: "image/jpeg" });
 
